10db6f58d8c8273f6a7ab9dfbd1f173ffb880b62
Esdras Lima <esdraslimaf@gmail.com>
2023-02-27 23:10:27 -0300

    Seedin Service

--- WebSalesMvc/Startup.cs
+++ WebSalesMvc/Startup.cs
@@ -40,14 +40,16 @@ namespace WebSalesMvc
                     options.UseMySql(Configuration.GetConnectionString("WebSalesMvcContext"), builder =>
                     builder.MigrationsAssembly("WebSalesMvc")));
 
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
